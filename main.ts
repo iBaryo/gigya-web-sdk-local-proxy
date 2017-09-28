f3f670dd76a1c2aa5db07dc7c4d6de645d8e2e86
Or Bar Yosef <or.ba@gigya-inc.com>
2017-09-28 18:52:44 +0300

    updating with global bin
    Commit by Or Bar Yosef <or.ba@gigya-inc.com>
    On branch: refs/heads/master

--- main.ts
+++ main.ts
@@ -4,6 +4,7 @@ import gigyaProxyMiddleware from "./src/middleware";
 const app = require('express')();
 const https = require('https');
 const fs = require('fs');
+const path = require('path');
 
 const consoleIlApiKey = '3_HkXvtGOzd1QMcVfHye7gcamXbLAoC77C4TCB7gk8mych-xEm5HTL0bCKBTNp56hk';
 const httpPort = 8080;
@@ -24,7 +25,7 @@ https://localhost:${httpsPort}/$2$3
 console.log('listening...');
 app.listen(httpPort);
 https.createServer({
-    cert: fs.readFileSync('./cert/cert.pem'),
-    key: fs.readFileSync('./cert/key.pem'),
+    cert: fs.readFileSync(path.join(__dirname, 'cert/cert.pem')),
+    key: fs.readFileSync(path.join(__dirname,'cert/key.pem')),
     passphrase: 'Gigya123'
 }, app).listen(httpsPort);
